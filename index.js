09d3b3c3bab070ab41ad64d78867cf13d376e0cb
MAIA332 <lk.maia0505@outlook.com>
2023-10-25 17:08:03 -0300

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+const path = require('path');
+const templates = "statics/templates/"
+
+var app = express();
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, templates+'index.html'));
+});
+
+app.listen(3000, function () {
+  console.log("App de Exemplo escutando na porta 3000!");
+});
